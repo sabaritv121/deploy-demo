6ebf74efd05b976213273fed3c60a2d938b1c07a
sabaritv121 <sabaritv121@gmail.com>
2023-11-20 16:41:01 +0530

    first commit

--- deploy/settings.py
+++ deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m#qqb@a!@eg#6&vh*dhe#tx@jj!+^%l!*r^r^30+bq=y%5=7^p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '4A*4GDfEG61DAD*5aaF*2FB2216fEebb',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '31036',
     }
 }
 
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- deploy/urls.py
+++ deploy/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('new_app.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
